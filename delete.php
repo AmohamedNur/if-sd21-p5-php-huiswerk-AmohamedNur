130607603d5fe06493f4a3a044a8f766b64594c3
AmohamedNur <302881042@student.rocmondriaan.nl>
2022-11-23 16:34:09 +0100

    delete pagina gemaakt

--- delete.php
+++ delete.php
@@ -1,1 +1,10 @@
 <?php
+$db=new PDO("mysql:host=localhost;dbname=fietsenmaker",
+    "root","");
+$query=$db->prepare("DELETE FROM fietsenmaker WHERE id=" .$_GET['id']);
+if($query->execute()){
+    header('Location: masterpagina.php');
+} else{
+    echo "error";
+}
+?>
