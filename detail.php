00b58453cbcce1d7fd161d718b4855a908db999d
AmohamedNur <302881042@student.rocmondriaan.nl>
2022-11-30 16:29:19 +0100

    tussentoets

--- detail.php
+++ detail.php
@@ -1,1 +1,43 @@
+<?php
+$db=new PDO("mysql:host=localhost;dbname=cijfersysteem",
+    "root","");
+$query=$db->prepare("SELECT * FROM cijfer");
+$query->execute();
+$result=$query->fetchAll(PDO::FETCH_ASSOC);
 
+echo "<style>
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+td, th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+
+tr:nth-child(even) {
+  background-color:#63c4bc;
+}
+</style>";
+
+echo "<table>";
+echo "<tr>";
+echo "<td>leerlingen</td>";
+echo "<td>vak</td>";
+echo "<td>cijfer</td>";
+echo "</tr>";
+foreach ($result as $data) {
+    echo "<tr>";
+    echo "<td>" .$data['id']."</td>";
+    echo "<td>" . $data['leerling'] . "</td>";
+    echo "<td>" . $data['vak'] . "</td>";
+    echo "<td>" . $data['cijfer'] . "</td>";
+    echo "<td> <a href='update.php?id=". $data['id'] . "'>update</a></td>";
+    echo "<td> <a href='delete.php?id=". $data['id'] . "'>delete</a></td>";
+    echo "</tr>";
+}
+echo "</table>";
+echo "<a href='insert.php'>insert</a>";
--- insert.php
+++ insert.php
@@ -1,20 +1,33 @@
 
 <?php
 session_start();
-if(isset($_POST['toevoegen'])) {
-    $_SESSION['merk']=$_POST['merk'];
-    $merk=$_SESSION['merk'];
-    $_SESSION['type']=$_POST['type'];
-    $type=$_SESSION['type'];
-    $_SESSION['prijs']=$_POST['prijs'];
-    $prijs=$_SESSION['prijs'];
-    $conn = new mysqli('localhost', 'root', '', 'fietsenmaker');
-    $stmt = $conn->prepare("insert into fietsenmaker (merk,type,prijs) values (?,?,?)");
-    $stmt->bind_param("ssi", $merk, $type, $prijs);
-    $stmt->execute();
-    $stmt->close();
-    $conn->close();
-    header('Location: masterpagina.php');
+try {
+    $db = new PDO("mysql:host=localhost;dbname=cijfersysteem", "root", "");
+
+if (isset($_POST['toevoegen'])){
+    $leerling = filter_input(INPUT_POST, "leerling", FILTER_SANITIZE_STRING);
+    $VAK = filter_input(INPUT_POST, "leerling", FILTER_SANITIZE_STRING);
+    $leerling = filter_input(INPUT_POST, "leerling", FILTER_SANITIZE_STRING);
+
+    if (empty($_POST ['leerling']) && empty($_POST['vak']) && empty($_POST['cijfer'])) {
+        echo "Vul je gegevens in !";
+    }
+    if (empty($_POST['cijfer'])){
+        echo "Vul jou komma getal!";
+    }
+
+    else{
+       $query = $db->prepare("INSERT INTO cijfer(leerling, vak, cijfer) VALUES(:leerling, :vak, :cijfer)");
+       $query->bindParam("leerling", $leerling);
+        $query->bindParam("vak", $vak);
+        $query->bindParam("cijfer", $cijfer);
+        $query->execute();
+        header('Location: datail.php');
+    }
+
+ }
+} catch (PDOException $e) {
+    die("error!:" . $e->getMessage());
 }
 ?><!DOCTYPE>
 <html>
@@ -25,18 +38,18 @@ if(isset($_POST['toevoegen'])) {
 <form method="post">
     <h3>Niuewe Merken Toevoegen</h3>
     <div class="mb-3">
-        <label for="formGroupExampleInput" class="form-label">Merk</label>
-        <input type="text" name="merk" class="form-control" id="formGroupExampleInput" value="">
+        <label for="formGroupExampleInput" class="form-label">Leerlijn</label>
+        <input type="text" name="leerling" class="form-control" id="formGroupExampleInput" value="">
     </div>
     <br>
     <div class="mb-3">
-        <label for="formGroupExampleInput2" class="form-label">Type</label>
-        <input type="text" name="type" class="form-control" id="formGroupExampleInput2" value="">
+        <label for="formGroupExampleInput2" class="form-label">Vak</label>
+        <input type="text" name="vak" class="form-control" id="formGroupExampleInput2" value="">
     </div>
     <br>
     <div class="mb-3">
-        <label for="formGroupExampleInput2" class="form-label">Prijs</label>
-        <input type="text" name="prijs" class="form-control" id="formGroupExampleInput2" value="">
+        <label for="formGroupExampleInput2" class="form-label">cijfer</label>
+        <input type="text" name="cijfer" class="form-control" id="formGroupExampleInput2" value="">
     </div>
     <br>
     <input type="submit" name="toevoegen" value="toevoegen">
